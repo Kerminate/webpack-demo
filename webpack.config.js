994aa5e48fdf747ef9ca6bc3742f3934e985d0a6
Kerminate <kepeilin@outlook.com>
2017-10-27 21:50:54 +0800

    build index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 var path = require('path')
+var htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  // entry: ['./src/script/main.js', './src/script/a.js'],
-  entry: './src/script/main.js',
+  entry: {
+    main: './src/script/main.js',
+    a: './src/script/a.js'
+  },
   output: {
-    path: path.resolve(__dirname, './dist/js'),
-    filename: 'bundle.js'
-  }
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name]-[chunkhash].js',
+    publicPath: 'http://cdn.com/'
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: false,
+      title: 'Webpack is good',
+      date: new Date(),
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    })
+  ]
 }
