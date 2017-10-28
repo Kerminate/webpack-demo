682efdf065661de0c346c372d98e4727787fe16c
Kerminate <kepeilin@outlook.com>
2017-10-28 22:42:45 +0800

    add html.css.less.image.loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'js/[name]-[chunkhash].js'
+    filename: 'js/[name].bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         use: {
@@ -16,6 +16,68 @@ module.exports = {
         },
         exclude: path.resolve(__dirname, 'node_modules'),
         include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /.tpl$/,
+        use: {
+          loader: 'ejs-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true, // Enable/Disable CSS Modules
+              importLoaders: 1 // Number of loaders applied before CSS loader
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
+      },
+      {
+        test: /.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'assets/[name]-[hash:5].[ext]',
+              limit: 200
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
       }
     ]
   },
